f1b842c5d190806a658fe489022e161568401950
Anna <anna.volna@gmail.com>
2017-06-11 14:41:17 +0200

    My Django Girls app, first commit

---
+++ Anicka.py
@@ -0,0 +1,15 @@
+def hi(name):
+    if name == "Anna":
+        print("Hi Anna!")
+    elif name == "Sonja":
+        print("Hi Sonja!")
+    else:
+        print("Hi anonymouse!")
+
+hi("Anna")
+def hi(name):
+    print ("Hi" + name + "!")
+girls = ["Anna", "Tom", "Bob"]
+for name in girls:
+        hi(name)
+        print("Next girl")
